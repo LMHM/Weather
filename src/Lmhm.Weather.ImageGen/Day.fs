b9c4fad653aaa6bea8e7fbc5290b219fff0e6962
Johan Olsson <jolsson@live.com>
2022-07-24 13:10:14 +0200

    Finish time axis background

--- src/Lmhm.Weather.ImageGen/Day.fs
+++ src/Lmhm.Weather.ImageGen/Day.fs
@@ -12,23 +12,54 @@ module private Process =
     let pixelsPerHour = 14
     let pixelsPerMinute = (float pixelsPerHour) / 60.0
 
-    let drawTimeAxis image =
+    let drawTimeDigits offsetY (hourOffsetX, image) =
+        let hourStr hour = hour % 24 |> sprintf "%02i"
+        let thirdHour (hour, _) = hour % 3 = 0
+        let draw image (hour, offsetX) = drawText (hourStr hour) Color.Cyan (offsetX, offsetY) image
+        (hourOffsetX, 
+            hourOffsetX
+            |> List.filter thirdHour
+            |> List.fold draw image)
+
+    let drawTimeTicks offsetY label (hourOffsetX, image) =
+        let pen hour = 
+            if hour % 3 = 0
+            then Pens.Solid(Color.DarkCyan, 1.0f)
+            else Pens.Dot(Color.DarkCyan, 1.0f)
+        let draw image (hour, offsetX) =
+            let x = offsetX + margin
+            drawLine (pen hour) (x, offsetY) (x, offsetY + 28) image
+        (hourOffsetX,
+            hourOffsetX
+            |> List.fold draw image
+            |> drawText label Color.Green (12, offsetY))
+
+    let last24hourOffsets() =
         let hourNow = DateTime.Now.Hour
         let minuteOffset = (float DateTime.Now.Minute) * pixelsPerMinute |> int
-        let pen hour = if hour % 3 = 0 then Pens.Solid(Color.DarkCyan, 1.0f) else Pens.Dot(Color.DarkCyan, 1.0f)
-        let hourStr hour = hour % 24 |> sprintf "%02i"
-        for hour in hourNow .. hourNow + 25 do
-            let offset = (hour - hourNow) * pixelsPerHour - minuteOffset
-            if offset >= 0 && offset <= 24 * pixelsPerHour then
-                let x = offset + margin
-                drawLine (pen hour) (x, 16) (x, 44) image |> ignore
-                if hour % 3 = 0 then 
-                    drawText (hourStr hour) Color.Cyan (offset, 2) image |> ignore
-        image
+        let toHourOffset hour = (hour, (hour - hourNow) * pixelsPerHour - minuteOffset)
+        let inDrawArea (_, offsetX) = offsetX >= 0 && offsetX <= 24 * pixelsPerHour
+        [ hourNow .. hourNow + 25 ]
+        |> List.map toHourOffset
+        |> List.filter inDrawArea
+ 
+    let drawTimeScales image =
+        (last24hourOffsets(), image)
+        |> drawTimeDigits 2
+        |> drawTimeTicks 16 "Riktn"
+        |> drawTimeTicks 48 "Hast" 
+        |> drawTimeTicks 80 "Tryck"
+        |> drawTimeTicks 112 "Temp"
+        |> drawTimeTicks 144 "RH"
+        |> drawTimeTicks 176 "Nbd"
+        |> drawTimeTicks 208 "Sikt"
+        |> drawTimeTicks 240 "KVLV"
+        |> drawTimeDigits 272
+        |> snd
 
     let generateImage data =
         loadImage "weather_template.png"
-        |> drawTimeAxis
+        |> drawTimeScales
 
 let readSensorData path =
     let today = DateOnly.FromDateTime(DateTime.Today)
