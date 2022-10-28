e4279818ce9de63b5888298ad9bb2b06d1571346
JohanO <jolsson@live.com>
2022-10-28 10:47:25 +0200

    Draw today and yesterday on year summary

--- src/Lmhm.Weather.ImageGen/Year.fs
+++ src/Lmhm.Weather.ImageGen/Year.fs
@@ -25,23 +25,24 @@ module Process =
         | Some v -> sprintf "%5.1f" v
         | None -> "  ---"
 
-    let drawToday (summary : DaySummary) image =
+    let drawDay color offset (summary : DaySummary) image =
         image
-        |> drawText (formatTemperature summary.TempMin) Color.White (72.0<px>, 240.0<px>)
-        |> drawText (formatTime summary.TempMin) Color.Grey (72.0<px>, 252.0<px>)
-        |> drawText (formatTemperature summary.TempMax) Color.White (144.0<px>, 240.0<px>)
-        |> drawText (formatTime summary.TempMax) Color.Grey (144.0<px>, 252.0<px>)
-        |> drawText (formatWind summary.WindMax) Color.White (232.0<px>, 240.0<px>)
-        |> drawText (formatTime summary.WindMax) Color.Grey (224.0<px>, 252.0<px>)
-        |> drawText (formatPressure summary.PressureMin) Color.White (312.0<px>, 240.0<px>)
-        |> drawText (formatTime summary.PressureMin) Color.Grey (320.0<px>, 252.0<px>)
-        |> drawText (formatPressure summary.PressureMax) Color.White (400.0<px>, 240.0<px>)
-        |> drawText (formatTime summary.PressureMax) Color.Grey (408.0<px>, 252.0<px>)
-        |> drawText (formatRain summary.Rain) Color.White (480.0<px>, 240.0<px>)
+        |> drawText (formatTemperature summary.TempMin) color (72.0<px>, 216.0<px> + offset)
+        |> drawText (formatTime summary.TempMin) Color.Grey (72.0<px>, 228.0<px> + offset)
+        |> drawText (formatTemperature summary.TempMax) color (144.0<px>, 216.0<px> + offset)
+        |> drawText (formatTime summary.TempMax) Color.Grey (144.0<px>, 228.0<px> + offset)
+        |> drawText (formatWind summary.WindMax) color (232.0<px>, 216.0<px> + offset)
+        |> drawText (formatTime summary.WindMax) Color.Grey (224.0<px>, 228.0<px> + offset)
+        |> drawText (formatPressure summary.PressureMin) color (312.0<px>, 216.0<px> + offset)
+        |> drawText (formatTime summary.PressureMin) Color.Grey (320.0<px>, 228.0<px> + offset)
+        |> drawText (formatPressure summary.PressureMax) color (400.0<px>, 216.0<px> + offset)
+        |> drawText (formatTime summary.PressureMax) Color.Grey (408.0<px>, 228.0<px> + offset)
+        |> drawText (formatRain summary.Rain) color (480.0<px>, 216.0<px> + offset)
 
     let generateImage data =
         loadImage "summary_template.png"
-        |> drawToday data.Today
+        |> drawDay Color.Yellow 0.0<px> data.Yesterday
+        |> drawDay Color.White 24.0<px> data.Today
 
 let run inputPath outputPath =
     DateOnly.FromDateTime(DateTime.Today)
